393919dacdcfa2a2ce0acf93afedaa891d0c90a6
Luigi-Alves <luigi.alves.carvalho@gmail.com>
2026-07-13 15:17:00 -0300

    Initial commit

---
+++ taskflow/src/main/java/com/taskflow/taskflow/model/Prioridade.java
@@ -0,0 +1,9 @@
+package com.taskflow.taskflow.model;
+
+public enum Prioridade {
+    BAIXA,
+    MEDIA,
+    ALTA,
+    MAXIMA
+}
+
---
+++ taskflow/src/main/java/com/taskflow/taskflow/model/Tarefa.java
@@ -0,0 +1,60 @@
+package com.taskflow.taskflow.model;
+
+import java.time.LocalDate;
+
+public class Tarefa {
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private Status status;
+    private Prioridade prioridade;
+    private LocalDate prazo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(LocalDate prazo) {
+        this.prazo = prazo;
+    }
+
+    public Prioridade getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
